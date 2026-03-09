d2d0c749aefdea4a0b8f6a7154551b28970ed4a0
lih8697-1 <lih8697@gmail.com>
2026-03-09 15:24:59 +0900

    43-1

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -25,7 +25,8 @@ public class Post extends BaseEntity {
 
     @OneToMany(mappedBy = "post",
             cascade = {CascadeType.PERSIST, CascadeType.REMOVE},
-            fetch = FetchType.LAZY)
+            fetch = FetchType.LAZY,
+            orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
 
     public Post(String title, String content) {
@@ -46,14 +47,19 @@ public class Post extends BaseEntity {
 
         return comment;
     }
+
     // 댓글 조회
-    public Optional<Comment> findCommentById(int commentId){
+    public Optional<Comment> findCommentById(int commentId) {
         return comments.stream()
                 .filter(c -> c.getId() == commentId)
                 .findFirst();
     }
 
     // 댓글 삭제
+    public void deleteComment(int id) {
+        Comment comment = findCommentById(id).get();
+        comments.remove(comment);
+    }
 
     // 댓글 수정
 }
